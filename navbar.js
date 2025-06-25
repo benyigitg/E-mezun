bac2a0e3cfe9f4ef961fd015e54ea8de507a2b9c
Yiğit Güler <105154268+benyigitg@users.noreply.github.com>
2025-06-25 21:21:53 +0300

    navbar.js

---
+++ navbar.js
@@ -0,0 +1,7 @@
+const navItems = [
+    {name:"Anasayfa" , link:"İndex.html"},
+    {name:"Matematik" , link:"Matematik.html"},
+    {name:"Fizik" , link:"Fizik.html"},
+    {name:"Kimya" , link:"Kimya.html"},
+    {name:"Biyoloji" , link:"Biyoloji.html"},
+    {name:"Blog" , link:"Blog.html"}];
